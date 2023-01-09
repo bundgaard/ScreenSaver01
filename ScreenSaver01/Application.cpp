f7c4a8bb9f9100c54bfb8f810d7078bd8b345cd9
David <bundgaard@noreply.github.com>
2023-01-09 16:08:55 +0100

    Moved all image related functions into Image class.

--- ScreenSaver01/Application.cpp
+++ ScreenSaver01/Application.cpp
@@ -1,18 +1,24 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <ScrnSave.h>
+#include <objbase.h>
 
-#include <algorithm>
+
 #include <strsafe.h>
+
+#include <ScrnSave.h>
 #pragma comment(lib, "Scrnsavw.lib")
+
 #pragma comment(lib, "Comctl32.lib")
 
-#include <d2d1.h>
-#pragma comment(lib, "d2d1.lib")
+
+#pragma region c++
+
+#include <algorithm>
 #include <fstream>
 #include <sstream>
-#include <wincodec.h>
-#pragma comment(lib, "windowscodecs.lib")
+
+#include "Image.h"
+#pragma endregion
 
 void ErrorExit()
 {
@@ -49,32 +55,13 @@ void ErrorExit()
 }
 
 
-// MUST BE DECLARED
-template <class T>
-void SafeRelease(T** ppT)
-{
-	if (*ppT)
-	{
-		(*ppT)->Release();
-		*ppT = NULL;
-	}
-}
-
 static TCHAR szAppName[APPNAMEBUFFERLEN] = L"ScreenSaver01";
 
 static size_t i = 0;
 static FLOAT x, y;
 std::wostringstream debug;
 
-static ID2D1Factory* pFactory = nullptr;
-static ID2D1HwndRenderTarget* pRenderTarget = nullptr;
-static ID2D1SolidColorBrush* pBrush = nullptr;
-static ID2D1Bitmap* pBitmap = nullptr;
-
-static IWICImagingFactory* pWicFactory = nullptr;
-static IWICBitmapDecoder* pBitmapDecoder = nullptr;
-static IWICBitmapFrameDecode* pBitmapFrameDecoder = nullptr;
-static IWICFormatConverter* pWicConverter = nullptr;
+scene::Image *image = nullptr;
 
 
 LRESULT WINAPI ScreenSaverProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -87,111 +74,14 @@ LRESULT WINAPI ScreenSaverProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam
 	case WM_CREATE:
 		{
 			HRESULT hr = S_OK;
-			hr = CoInitialize(nullptr);
+			hr = ::CoInitialize(nullptr);
 			if (FAILED(hr))
 			{
 				debug << L"failed to co initialize" << std::endl;
 				return false;
-			}
+			}	
 
-
-			hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, _uuidof(ID2D1Factory),
-			                       reinterpret_cast<void**>(&pFactory));
-			if (FAILED(hr))
-			{
-				debug << L"failed to create d2d1 factory" << std::endl;
-				return FALSE;
-			}
-			GetClientRect(hwnd, &rct);
-			const auto width = rct.right - rct.left;
-			const auto height = rct.bottom - rct.top;
-			hr = pFactory->CreateHwndRenderTarget(
-				D2D1::RenderTargetProperties(),
-				D2D1::HwndRenderTargetProperties(
-					hwnd,
-					D2D1::SizeU(width, height)),
-				&pRenderTarget);
-			if (FAILED(hr))
-			{
-				debug << L"failed to create hwnd render target" << std::endl;
-				return FALSE;
-			}
-
-			hr = pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &pBrush);
-			if (FAILED(hr))
-			{
-				debug << L"failed to create solid color brush" << std::endl;
-				return FALSE;
-			}
-
-			// Create the COM imaging factory
-			hr = CoCreateInstance(
-				CLSID_WICImagingFactory,
-				nullptr,
-				CLSCTX_INPROC_SERVER,
-				IID_PPV_ARGS(&pWicFactory)
-			);
-			if (FAILED(hr))
-			{
-				debug << L"failed to create wic factory" << std::endl;
-				return FALSE;
-			}
-
-			hr = pWicFactory->CreateDecoderFromFilename(
-				L"C:\\Code\\test2.png",
-				nullptr,
-				GENERIC_READ,
-				WICDecodeMetadataCacheOnLoad, &pBitmapDecoder);
-			if (FAILED(hr))
-			{
-				debug << L"failed to create decoder from filename" << std::endl;
-				return FALSE;
-			}
-
-			hr = pBitmapDecoder->GetFrame(0, &pBitmapFrameDecoder);
-			if (FAILED(hr))
-			{
-				debug << L"failed to get frame" << std::endl;
-				return FALSE;
-			}
-			
-			if (pBitmapFrameDecoder == nullptr)
-			{
-				debug << L"bitmap frame decoder is null" << std::endl;
-			}
-			UINT w, h;
-			hr = pBitmapFrameDecoder->GetSize(&w, &h);
-			if (SUCCEEDED(hr))
-			{
-				debug << L"Width " << w << L" " << L"Height " << h << std::endl;
-			}
-
-			// Before this can happen we need to convert
-
-			hr = pWicFactory->CreateFormatConverter(&pWicConverter);
-			if (SUCCEEDED(hr))
-			{
-				hr = pWicConverter->Initialize(
-					pBitmapFrameDecoder,
-					GUID_WICPixelFormat32bppPBGRA,
-					WICBitmapDitherTypeNone,
-					nullptr,
-					0.0,
-					WICBitmapPaletteTypeMedianCut);
-				if (SUCCEEDED(hr))
-				{
-					hr = pRenderTarget->CreateBitmapFromWicBitmap(
-						pWicConverter,
-						&pBitmap);
-					if (SUCCEEDED(hr))
-					{
-						//FormatMessage()
-
-						debug << L"created bitmap from wic bitmap" << std::endl;
-					}
-				}
-			}
-
+			image = new scene::Image(hwnd);
 
 			timerID = SetTimer(hwnd, 1, 25, nullptr);
 		}
@@ -200,18 +90,8 @@ LRESULT WINAPI ScreenSaverProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam
 		return TRUE;
 	case WM_TIMER:
 		{
-			pRenderTarget->BeginDraw();
-			pRenderTarget->SetTransform(D2D1::IdentityMatrix());
-			pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
-			pRenderTarget->DrawBitmap(
-				pBitmap,
-				D2D1::RectF(
-					100.0f + x,
-					100.0f + y,
-					640.f,
-					480.0f));
 
-			pRenderTarget->EndDraw();
+			image->Draw(x,y);
 			x += 0.5f;
 			y += 0.5f;
 		}
@@ -221,14 +101,10 @@ LRESULT WINAPI ScreenSaverProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam
 			if (timerID)
 				KillTimer(hwnd, timerID);
 
-			SafeRelease(&pWicConverter);
-			SafeRelease(&pBrush);
-			SafeRelease(&pRenderTarget);
-			SafeRelease(&pFactory);
-			SafeRelease(&pWicFactory);
-			SafeRelease(&pBitmapDecoder);
-			SafeRelease(&pBitmapFrameDecoder);
-			SafeRelease(&pBitmap);
+			if(image != nullptr)
+			{
+				delete image;
+			}
 			CoUninitialize();
 
 			std::wfstream out;
@@ -254,6 +130,7 @@ BOOL WINAPI ScreenSaverConfigureDialog(HWND hDlg, UINT msg, WPARAM wParam, LPARA
 		
 		if(wParam== IDOK)
 		{
+			
 			// Find image
 		}else if (wParam == IDCANCEL)
 		{
---
+++ ScreenSaver01/Image.h
@@ -0,0 +1,167 @@
+﻿#pragma once
+
+#include <d2d1.h>
+#pragma comment(lib, "d2d1.lib")
+#include <wincodec.h>
+#pragma comment(lib, "windowscodecs.lib")
+// MUST BE DECLARED
+template <class T>
+void SafeRelease(T** ppT)
+{
+	if (*ppT)
+	{
+		(*ppT)->Release();
+		*ppT = NULL;
+	}
+}
+
+namespace scene
+{
+	class Image
+	{
+		HWND Hwnd;
+		RECT ClientRect{};
+
+		ID2D1Factory* pFactory = nullptr;
+		ID2D1HwndRenderTarget* pRenderTarget = nullptr;
+		ID2D1SolidColorBrush* pBrush = nullptr;
+		ID2D1Bitmap* pBitmap = nullptr;
+
+		IWICImagingFactory* pWicFactory = nullptr;
+		IWICBitmapDecoder* pBitmapDecoder = nullptr;
+		IWICBitmapFrameDecode* pBitmapFrameDecoder = nullptr;
+		IWICFormatConverter* pWicConverter = nullptr;
+
+		bool Init()
+		{
+			HRESULT hr = S_OK;
+			hr = D2D1CreateFactory(
+				D2D1_FACTORY_TYPE_SINGLE_THREADED,
+				_uuidof(ID2D1Factory),
+				reinterpret_cast<void**>(&pFactory));
+			if (FAILED(hr))
+			{
+				return false;
+			}
+
+			GetClientRect(Hwnd, &ClientRect);
+			const auto width = ClientRect.right - ClientRect.left;
+			const auto height = ClientRect.bottom - ClientRect.top;
+
+			hr = pFactory->CreateHwndRenderTarget(
+				D2D1::RenderTargetProperties(),
+				D2D1::HwndRenderTargetProperties(
+					Hwnd,
+					D2D1::SizeU(width, height)),
+				&pRenderTarget);
+			if (FAILED(hr))
+			{
+				return false;
+			}
+
+			hr = pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &pBrush);
+			if (FAILED(hr))
+			{
+				return false;
+			}
+
+			hr = CoCreateInstance(
+				CLSID_WICImagingFactory,
+				nullptr,
+				CLSCTX_INPROC_SERVER,
+				IID_PPV_ARGS(&pWicFactory)
+			);
+			if (FAILED(hr))
+			{
+				return false;
+			}
+
+			hr = pWicFactory->CreateDecoderFromFilename(
+				L"C:\\Code\\test2.png",
+				nullptr,
+				GENERIC_READ,
+				WICDecodeMetadataCacheOnLoad, &pBitmapDecoder);
+			if (FAILED(hr))
+			{
+				return false;
+			}
+
+			hr = pBitmapDecoder->GetFrame(0, &pBitmapFrameDecoder);
+			if (FAILED(hr))
+			{
+				return false;
+			}
+
+
+			UINT w, h;
+			hr = pBitmapFrameDecoder->GetSize(&w, &h);
+			if (!SUCCEEDED(hr))
+			{
+				return false;
+			}
+
+			// Before this can happen we need to convert
+
+			hr = pWicFactory->CreateFormatConverter(&pWicConverter);
+			if (SUCCEEDED(hr))
+			{
+				hr = pWicConverter->Initialize(
+					pBitmapFrameDecoder,
+					GUID_WICPixelFormat32bppPBGRA,
+					WICBitmapDitherTypeNone,
+					nullptr,
+					0.0,
+					WICBitmapPaletteTypeMedianCut);
+				if (SUCCEEDED(hr))
+				{
+					hr = pRenderTarget->CreateBitmapFromWicBitmap(
+						pWicConverter,
+						&pBitmap);
+					if (SUCCEEDED(hr))
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+	public:
+		explicit Image(HWND hwnd) : Hwnd(hwnd)
+		{
+			if (!Init())
+			{
+			}
+		}
+
+		~Image()
+		{
+			SafeRelease(&pWicConverter);
+			SafeRelease(&pBrush);
+			SafeRelease(&pRenderTarget);
+			SafeRelease(&pFactory);
+			SafeRelease(&pWicFactory);
+			SafeRelease(&pBitmapDecoder);
+			SafeRelease(&pBitmapFrameDecoder);
+			SafeRelease(&pBitmap);
+		}
+
+
+		void Draw(FLOAT x, FLOAT y)
+		{
+			pRenderTarget->BeginDraw();
+			pRenderTarget->SetTransform(D2D1::IdentityMatrix());
+			pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+			pRenderTarget->DrawBitmap(
+				pBitmap,
+				D2D1::RectF(
+					100.0f + x,
+					100.0f + y,
+					640.f,
+					480.0f));
+
+			pRenderTarget->EndDraw();
+		}
+	};
+}
